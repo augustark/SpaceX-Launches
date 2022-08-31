0fecac51e1c75b7498d5f89719bad5643083e43b
Neo <neofranco9@gmail.com>
2022-08-31 15:58:10 +0800

    can't finished implementing lazy load on time

--- src/App.js
+++ src/App.js
@@ -5,18 +5,46 @@ import './App.scss'
 function App() { 
   const [searchQuery, setSearchQuery] = useState('')
   const [launchesData, setLaunchesData] = useState([])
+  // const [pageNumber, setPageNumber] = useState(1)
 
   useEffect(() => {
+    // const fetchData = async () => {
+    //   const res = await fetch(`https://api.spacexdata.com/v5/launches/query`, {
+    //     method: 'POST',
+    //     body: {
+    //       query: searchQuery,
+    //       options: {
+    //         page: pageNumber
+    //       }
+    //     }
+    //   })
+    //   const data = await res.json()
+    //   setLaunchesData(prev => ([
+    //     ...prev,
+    //     ...data.docs
+    //   ]))
+    //   setPageNumber((previousNumber) => previousNumber + 1)
+    // }
+  
+    // const handleScroll = (e) => {
+    //   if (window.innerHeight + e.target.documentElement.scrollTop === e.target.documentElement.scrollHeight) {
+    //     fetchData()
+    //   }
+    // }
+
     const fetchData = async () => {
-      const res = await fetch('https://api.spacexdata.com/v5/launches')
+      const res = await fetch(`https://api.spacexdata.com/v5/launches`)
       const data = await res.json()
       setLaunchesData(data)
     }
+
     fetchData()
+    // window.addEventListener('scroll', handleScroll)
   }, [])
 
   const handleChange = (e) => {
       setSearchQuery(e.target.value)
+      // setPageNumber(1)
   }
 
   return (
@@ -40,7 +68,7 @@ function App() {
                   return item
                 } else if (item.name.toLowerCase().includes(searchQuery.toLowerCase())) {
                   return item
-                }
+                } 
               })
               .map((item, index) => {
                 const event = new Date(item.date_local).getFullYear()
